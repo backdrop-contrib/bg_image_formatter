168c14ff73b628906bf489526d952a1f84df48e5
indigoxela <user@olga.indigofloat.at>
2022-06-08 17:02:27 +0200

    Different approach for the text color by css class

---
+++ js/bg-image-formatter.js
@@ -0,0 +1,24 @@
+(function ($) {
+
+  'use strict';
+
+  Backdrop.behaviors.bgImageFormatter = {
+    attach: function (context, settings) {
+      var selectors = settings.bg_image_formatter.selectors;
+      for (let i = 0; i < selectors.length; i++) {
+        var element = document.querySelector(selectors[i]);
+        var style = getComputedStyle(element).backgroundColor;
+        var rgb = style.substring(style.indexOf('(') + 1, style.length - 1).split(', ');
+        var avg = (Number(rgb[0]) + Number(rgb[1]) + Number(rgb[2])) / 3;
+
+        if (avg > 128) {
+          element.classList.add('bg-color-light');
+        }
+        else {
+          element.classList.add('bg-color-dark');
+        }
+      }
+    }
+  }
+
+})(jQuery);
