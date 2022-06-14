1ebab80c5f506436f16d6e2d7a370466278a9a2c
indigoxela <user@olga.indigofloat.at>
2022-06-14 10:36:22 +0200

    Attach js only once, defensive coding

--- js/bg-image-formatter.js
+++ js/bg-image-formatter.js
@@ -8,6 +8,10 @@
       for (let i = 0; i < selectors.length; i++) {
         var element = document.querySelector(selectors[i]);
         var style = getComputedStyle(element).backgroundColor;
+        if (style.indexOf('rgba') === 0) {
+          // We define rgb, so if it's rgba, no background color was set.
+          continue;
+        }
         var rgb = style.substring(style.indexOf('(') + 1, style.length - 1).split(', ');
         var avg = (Number(rgb[0]) + Number(rgb[1]) + Number(rgb[2])) / 3;
 
