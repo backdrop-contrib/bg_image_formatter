182fafe94ceba8b77a0cb30273687d2c8a0a22b8
indigoxela <user@olga.indigofloat.at>
2025-04-25 12:48:11 +0200

    Issue #11: Make bgImageFormatter more robust by @josarnold

--- js/bg-image-formatter.js
+++ js/bg-image-formatter.js
@@ -1,20 +1,28 @@
 (function ($) {
-
   'use strict';
 
   Backdrop.behaviors.bgImageFormatter = {
     attach: function (context, settings) {
-      var selectors = settings.bg_image_formatter.selectors;
+      if (!settings.bg_image_formatter || !settings.bg_image_formatter.selectors) {
+        return;
+      }
+
+      const selectors = settings.bg_image_formatter.selectors;
       for (let i = 0; i < selectors.length; i++) {
-        var element = document.querySelector(selectors[i]);
-        var style = getComputedStyle(element).backgroundColor;
+        const element = document.querySelector(selectors[i]);
+        if (!element) {
+          // No such element, nothing to do.
+          continue;
+        }
+
+        const style = getComputedStyle(element).backgroundColor;
         if (style.indexOf('rgba') === 0) {
           // We define rgb, so if it's rgba, no background color was set.
           continue;
         }
-        var rgb = style.substring(style.indexOf('(') + 1, style.length - 1).split(', ');
-        var avg = (Number(rgb[0]) + Number(rgb[1]) + Number(rgb[2])) / 3;
 
+        const rgb = style.substring(style.indexOf('(') + 1, style.length - 1).split(', ');
+        const avg = (Number(rgb[0]) + Number(rgb[1]) + Number(rgb[2])) / 3;
         if (avg > 128) {
           element.classList.add('bg-color-light');
         }
@@ -23,6 +31,6 @@
         }
       }
     }
-  }
+  };
 
 })(jQuery);
